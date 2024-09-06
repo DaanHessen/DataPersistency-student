0a87c121ffed405473e3ef58f9a261ddd2a01267
Daan Hessen <33068305+DaanHessen@users.noreply.github.com>
2024-09-06 13:47:44 +0200

    made assignment S1_DDL_student

--- opdracht/S1_DDL_student.sql
+++ opdracht/S1_DDL_student.sql
@@ -35,6 +35,9 @@
 -- geaccepteerd. Test deze regel en neem de gegooide foutmelding op als
 -- commentaar in de uitwerking.
 
+ALTER TABLE medewerkers ADD COLUMN geslacht CHAR(1); -- voegt kolom 'geslacht' toe aan de medewerkerstabel, met een waarde van 1 karakter
+ALTER TABLE medewerkers ADD CONSTRAINT m_geslacht_chk CHECK (geslacht IN ('M', 'V')); -- voegt een beperkingsregel toe die alleen 'M' of 'V' als geldige waarde accepteert
+
 
 -- S1.2. Nieuwe afdeling
 --
@@ -44,16 +47,30 @@
 -- en valt direct onder de directeur.
 -- Voeg de nieuwe afdeling en de nieuwe medewerker toe aan de database.
 
+INSERT INTO afdelingen (anr, naam, locatie, hoofd) VALUES (-, 'ONDERZOEK', 'ZWOLLE', -); -- voegt de nieuwe afdeling in zwolle toe
+INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm, afd) VALUES (-, 'DONK', 'A', 'DIRECTEUR', -, -, -, -, -); -- voegt A. Donk toe aan de DB
+
 
 -- S1.3. Verbetering op afdelingentabel
 --
 -- We gaan een aantal verbeteringen doorvoeren aan de tabel `afdelingen`:
 --   a) Maak een sequence die afdelingsnummers genereert. Denk aan de beperking
 --      dat afdelingsnummers veelvouden van 10 zijn.
+
+CREATE SEQUENCE afdelingen_anr_sequence START WITH 10 INCREMENT BY 10; -- maakt een sequence aan die afdelingsnummers genereert, begint bij 10 en telkeens verhoogt met 10
+
 --   b) Voeg een aantal afdelingen toe aan de tabel, maak daarbij gebruik van
 --      de nieuwe sequence.
+
+INSERT INTO afdelingen (anr, naam, locatie, hoofd) VALUES (nextval('afdelingen_anr_sequence'), 'ADMINISTRATIE', 'UTRECHT', -); -- voegt 2 nieuwe afdelingen toe met de gegenereerde afdelingsnummers
+INSERT INTO afdelingen (anr, naam, locatie, hoofd) VALUES (nextval('afdelingen_anr_sequence'), 'LOGISTIEK', 'AMSTERDAM', -);
+
+
 --   c) Op enig moment gaat het mis. De betreffende kolommen zijn te klein voor
 --      nummers van 3 cijfers. Los dit probleem op.
+
+ALTER TABLE afdelingen ALTER COLUMN anr TYPE NUMERIC(3); -- verandert de kolom naar een numerieke waarde van 3 cijfers ipv 2
+
 
 
 -- S1.4. Adressen
@@ -69,6 +86,18 @@
 --    telefoon      10 cijfers, uniek
 --    med_mnr       FK, verplicht
 
+CREATE TABLE 'adressen' (
+    postcode CHAR(6) CONSTRAINT postcode_format CHECK (postcode ~ '^\d{4}[A-Z]{2}$'), -- de constraint zorgt ervoor dat er 4 cijfers mogen zijn en twee letters.
+    huisnummer CHAR(6),
+    ingangsdatum DATE,
+    einddatum DATE CONSTTAINT einddatum_check CHECK (einddatum > ingangsdatum), -- constraint checkt of de einddatum niet voor de ingangdatum ligt,
+    telefoon CHAR(10) UNIQUE, -- UNIQUE zorgt ervoor dat elk telefoonnr uniek is
+    med_mnr NUMERIC(4),
+    PRIMARY KEY (postcode, huisnummer, ingangsdatum),
+    FOREIGN KEY (med_mnr) REFERENCES medewerkers (mnr)
+);
+)
+
 
 -- S1.5. Commissie
 --
@@ -82,7 +111,14 @@ VALUES (8001, 'MULLER', 'TJ', 'TRAINER', 7566, '1982-08-18', 2000, 500);
 INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm)
 VALUES (8002, 'JANSEN', 'M', 'VERKOPER', 7698, '1981-07-17', 1000, NULL);
 
-
+ALTER TABLE medewerkers -- voegt constraint comm_check toe die checkt of kolom comm NULL is als functie niet VERKOPER is en vice versa
+ADD CONSTRAINT comm_check
+CHECK (
+    (functie = 'VERKOPER' AND comm IS NOT NULL)
+    OR
+    (functie != 'VERKOPER' AND comm IS NULL)
+);
+    )
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
 -- Met onderstaande query kun je je code testen. Zie bovenaan dit bestand
