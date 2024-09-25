a0840a1b5986d77f15b77ed25b52c4dffab69e5e
Daan Hessen <33068305+DaanHessen@users.noreply.github.com>
2024-09-25 15:08:29 +0200

    de datums zijn verkeerd geformatteerd, kwam ik net achter (ik kan niet testen, kom er dus nu pas achter)

--- opdracht/S3_Multiple_Tables_student.sql
+++ opdracht/S3_Multiple_Tables_student.sql
@@ -31,11 +31,24 @@
 -- code, de begindatum, de lengte en de naam van de docent.
 -- DROP VIEW IF EXISTS s3_1; CREATE OR REPLACE VIEW s3_1 AS                                                     -- [TEST]
 
+    CREATE OR REPLACE VIEW s3_1 AS
+           SELECT c.code, c.lengte, u.begindatum, m.naam AS docent
+        FROM cursussen c
+            JOIN uitvoeringen u ON c.code = u.cursus
+            JOIN medewerkers m ON u.docent = m.mnr;
+
 
 -- S3.2.
 -- Geef in twee kolommen naast elkaar de achternaam van elke cursist (`cursist`)
 -- van alle S02-cursussen, met de achternaam van zijn cursusdocent (`docent`).
 -- DROP VIEW IF EXISTS s3_2; CREATE OR REPLACE VIEW s3_2 AS                                                     -- [TEST]
+
+    CREATE OR REPLACE VIEW s3_2 AS
+           SELECT i.cursist AS cursist, m.naam AS docent
+        FROM inschrijvingen i
+            JOIN uitvoeringen u ON i.cursus = u.cursus
+            JOIN medewerkers m ON u.docent = m.mnr
+            WHERE u.cursus LIKE 'S02%';
 
 
 -- S3.3.
@@ -43,23 +56,42 @@
 -- afdeling (`hoofd`).
 -- DROP VIEW IF EXISTS s3_3; CREATE OR REPLACE VIEW s3_3 AS                                                     -- [TEST]
 
+    CREATE OR REPLACE VIEW s3_3 AS
+           SELECT a.hoofd AS afdeling, m.mnr AS hoofd
+        FROM afdelingen a
+            JOIN medewerkers m ON a.hoofd = m.mnr;
+
 
 -- S3.4.
 -- Geef de namen van alle medewerkers, de naam van hun afdeling (`afdeling`)
 -- en de bijbehorende locatie.
 -- DROP VIEW IF EXISTS s3_4; CREATE OR REPLACE VIEW s3_4 AS                                                     -- [TEST]
 
+    CREATE OR REPLACE VIEW s3_4 AS
+           SELECT m.naam as medewerker, a.naam as afdeling, a.locatie
+           FROM medewerkers m
+              JOIN afdelingen a ON m.afd = a.anr;
+
 
 -- S3.5.
 -- Geef de namen van alle cursisten die staan ingeschreven voor de cursus S02 van 12 april 2019
 -- DROP VIEW IF EXISTS s3_5; CREATE OR REPLACE VIEW s3_5 AS                                                     -- [TEST]
 
+    CREATE OR REPLACE VIEW s3_5 AS
+           SELECT m.naam AS cursist
+           FROM medewerkers m
+              JOIN inschrijvingen i ON m.mnr = i.cursist
+              JOIN uitvoeringen u ON i.cursus = u.cursus
+           WHERE u.cursus = 'S02' AND u.begindatum = '12-04-2019';
 
 -- S3.6.
 -- Geef de namen van alle medewerkers en hun toelage.
 -- DROP VIEW IF EXISTS s3_6; CREATE OR REPLACE VIEW s3_6 AS                                                     -- [TEST]
 
-
+    CREATE OR REPLACE VIEW s3_6 AS
+           SELECT m.naam AS medewerker, s.toelage
+           FROM medewerkers m
+                JOIN schalen s ON m.snr = s.snr;
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
 -- Met onderstaande query kun je je code testen. Zie bovenaan dit bestand
